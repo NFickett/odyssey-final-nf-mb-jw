1c3569f526840c379a2ff8f1050e943b93336045
Nathan Fickett <nathanefickett@lewisu.edu>
2020-12-04 11:39:59 -0600

    Added a test button for email and image

---
+++ public/libs/login.js
@@ -0,0 +1,33 @@
+function login(){
+    window.open("https://odyssey-final-nf-mb-jw.azurewebsites.net/.auth/login/google/callback")
+}
+
+function getEmail(){
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET','https://odyssey-final-nf-mb-jw.azurewebsites.net/.auth/me');
+    ourRequest.onload = function(){
+        userData = JSON.parse(ourRequest.responseText);
+        console.log(userData[0].user_id);
+
+        document.getElementById("emailTest").insertAdjacentHTML('beforeend', userData[0].user_id);
+    };
+    ourRequest.send();
+};
+
+function getImage(){
+    
+    var imageLink = document.createElement('img');
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET','https://odyssey-final-nf-mb-jw.azurewebsites.net/.auth/me');
+    ourRequest.onload = function(){
+        userData = JSON.parse(ourRequest.responseText);
+        for(i = 0; userData[0].user_claims[i]; i++){
+            if (userData[0].user_claims[i].typ = "picture"){
+                imageLink.src = userData[0].user_claims[i].val
+            }
+        }
+
+        document.getElementById("imageTest").appendChild(imageLink);
+    };
+    ourRequest.send();
+};
