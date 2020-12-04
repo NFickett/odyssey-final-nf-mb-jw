82372d5a3847809d5e2a5ed5b6efbe77c20cd6d3
Nathan Fickett <nathanefickett@lewisu.edu>
2020-12-04 11:46:02 -0600

    Added a test button for email and image

--- public/libs/login.js
+++ public/libs/login.js
@@ -24,9 +24,9 @@ function getImage(){
         for(i = 0; userData[0].user_claims[i]; i++){
             if (userData[0].user_claims[i].typ = "picture"){
                 imageLink.src = userData[0].user_claims[i].val
+                console.log(userData[0].user_claims[i].val)
             }
         }
-
         document.getElementById("imageTest").appendChild(imageLink);
     };
     ourRequest.send();
