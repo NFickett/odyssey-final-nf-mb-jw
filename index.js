2678a10ca4288094ab1c947ac8cbf4cbc0f32ecf
mfbernasol <mfbernasol@gmail.com>
2020-12-11 12:51:54 -0600

    change file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+var path = require('path');
 require('dotenv').config()
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
